b03a922796abd2e11f447ae647062019aced088c
Alexandre Freitas Silva <alexandrefreitasx@outlook.com>
2025-03-27 09:44:30 -0300

    Add files via upload

---
+++ Aula02/desafio.js
@@ -0,0 +1,6 @@
+var nomeCarro = prompt("Qual o nome do seu carro?")
+var anoCarro = prompt("Qual o ano do seu carro?")
+var anoAtual = prompt("Qual o ano atual?")
+
+var conta = anoAtual - anoCarro
+alert("Seu carro é um " + nomeCarro + " do ano " + anoCarro + " e fará " + conta + " anos");
